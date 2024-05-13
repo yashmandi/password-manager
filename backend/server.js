740f9aea9c7492c415abcb071b4f75cdab235811
yashmandi <yashmandi18@gmail.com>
2024-05-13 11:37:24 +0530

    backend route

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,44 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const dotenv = require('dotenv')
+const { MongoClient } = require('mongodb');
+const bodyParser = require("body-parser");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+dotenv.config();
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+const dbName = 'safe_cypher';
+const app = express();
+const port = 3000
+app.use(bodyParser.json());
+
+client.connect();
+
+// get all passwords
+app.get('/', async (req, res) => {
+    const db = client.db(dbName);
+    const collection = db.collection('passwords');
+    const findResult = await collection.find({}).toArray();
+    res.json(findResult);
+})
+
+// save password    
+app.post('/', async (req, res) => {
+    const password = req.body;
+    const db = client.db(dbName);
+    const collection = db.collection('passwords');
+    const findResult = await collection.insertOne(password);
+    res.send({ success: true });
+})
+
+// delete password
+app.post('/', async (req, res) => {
+    const password = req.body;
+    const db = client.db(dbName);
+    const collection = db.collection('passwords');
+    const findResult = await collection.deleteOne(password)
+    res.send({ success: true });
 })
 
 app.listen(port, () => {
